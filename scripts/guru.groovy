c3bac1f33c5fb8664066c27d59cb9cc4a34540b5
Jakob Bornecrantz <wallbraker@gmail.com>
2017-09-11 13:55:11 +0200

    guru: Add Guru script

---
+++ scripts/guru.groovy
@@ -0,0 +1,63 @@
+@Library('volt-build')
+import org.volt_lang.Builder
+import org.volt_lang.RepoConf
+
+def b = new Builder(steps)
+
+stage('Setup') {
+	b.setup()
+	b.replaceRepos([
+		new RepoConf('guru', 'https://github.com/VoltLang/Guru', false),
+		new RepoConf('metal', 'https://github.com/VoltLang/Metal', false)
+	])
+}
+
+stage('Checkout') {
+	b.checkoutAll()
+}
+
+stage('Toolchain') {
+	def fileTool = "toolchain-x86_64-linux.tar.gz"
+	def fileDiode = "diode-x86_64-linux.tar.gz"
+	def fileSources = "sources.tar.gz"
+
+	node('master') {
+		dir('staging') {
+			deleteDir()
+			step([$class: 'CopyArtifact', filter: fileTool, fingerprintArtifacts: true, projectName: 'Volt'])
+			step([$class: 'CopyArtifact', filter: fileDiode, fingerprintArtifacts: true, projectName: 'Volt'])
+			step([$class: 'CopyArtifact', filter: fileSources, fingerprintArtifacts: true, projectName: 'Volt'])
+			sh """
+			tar xfv ${fileTool}
+			tar xfv ${fileDiode}
+			strip diode
+			mv diode bin/diode
+			tar xfv ${fileSources}
+			rm ${fileTool} ${fileDiode} ${fileSources}
+			rm -rf lib
+			rsync -r -v --checksum --delete --exclude ".git" ../guru/ guru
+			rsync -r -v --checksum --delete --exclude ".git" ../metal/ metal
+			"""
+		}
+	}
+}
+
+stage('Generate') {
+	node('master') {
+		def jsonDir = "${pwd tmp: true}/json"
+		def outputDir = "${pwd tmp: true}/output"
+		def buildDir = "${pwd}/build"
+		sh """
+		mkdir -p ${jsonDir}
+		rm -rf ${outputDir}
+		mkdir -p ${outputDir}
+		rsync -r -v --checksum --delete staging/ ${buildDir}
+		make -C ${buildDir} -f guru/_scripts/GNUmakefile \
+			JSON_DIR=${jsonDir} \
+			OUTPUT_DIR=${outputDir} \
+			VOLTA=bin/volta \
+			DIODE=bin/diode
+		rsync -r -v --checksum --delete ${outputDir}/ /opt/volt.guru
+		"""
+	}
+}
